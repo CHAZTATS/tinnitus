362048ff6981f09e447eda9888fa50c3f021565e
Charles Taverner <charles.taverner@domesticandgeneral.com>
2026-04-21 16:09:40 +0100

    ios ads on

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   adMob: {
-    iosBannerAdUnitId: '',
+    iosBannerAdUnitId: 'ca-app-pub-3106052247964578/8181931902',
     androidBannerAdUnitId: 'ca-app-pub-3106052247964578/2648735323',
     testing: false,
   },
